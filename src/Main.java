7d3631dab6ec918caeb642349df991339c96147b
Benjamin <benjamin@smoerfugl.dk>
2017-09-14 14:11:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    // en række tekst
+    public static void main(String[] args) {
+        System.out.println("welcome to java");
+        System.out.println("ill show you something fun");
+        System.out.println("it might hurt you heart");
+        System.out.println("consider yourself warned");
+
+        // int er en variabel givet som tal, string er en variabel givet so tekst
+        int etTal = 4;
+        String entekst = "hejsa";
+
+        // et loop som tæller op hver gang den køre rundt
+        for (int i = 0; i < 6; i++){
+                System.out.println(i+1);
+                System.out.println("hejsa");
+        }
+    }
+}
